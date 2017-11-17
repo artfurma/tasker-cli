bd1e4d6ea210a077cb7a8f564db8953701c4de4b
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-11-17 21:56:24 +0100

    Reformat layout

--- src/app/tasker-material.module.ts
+++ src/app/tasker-material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatCardModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatCardModule, MatListModule, FlexLayoutModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatCardModule, MatListModule, FlexLayoutModule],
 })
 export class TaskerMaterialModule { }
