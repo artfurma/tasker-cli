09e357a339e722b9f9113748104ddc9b6d6a186e
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-11-22 20:17:31 +0100

    Fix formatting in tasker-material module

--- src/app/tasker-material.module.ts
+++ src/app/tasker-material.module.ts
@@ -1,9 +1,42 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule,MatSelectModule, MatToolbarModule,MatChipsModule, MatIconModule, MatCardModule, MatListModule,MatInputModule } from '@angular/material';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatInputModule
+} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule,MatSelectModule, MatChipsModule, MatCardModule, MatToolbarModule, MatIconModule, MatCardModule, MatListModule, FlexLayoutModule,MatInputModule],
-    exports: [MatButtonModule, MatCheckboxModule,MatSelectModule, MatChipsModule, MatCardModule, MatToolbarModule, MatIconModule, MatCardModule, MatListModule, FlexLayoutModule,MatInputModule],
+    imports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        MatChipsModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatListModule,
+        FlexLayoutModule,
+        MatInputModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        MatChipsModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatListModule,
+        FlexLayoutModule,
+        MatInputModule],
 })
 export class TaskerMaterialModule { }
