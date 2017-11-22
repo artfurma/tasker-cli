464d94ec71f68d4f5b1d7d8177bb7b9974d91878
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-11-23 00:10:32 +0100

    Hide navbar when logged out

--- src/app/auth/authentication/authentication.service.ts
+++ src/app/auth/authentication/authentication.service.ts
@@ -4,23 +4,27 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { HttpClientModule } from '@angular/common/http';
-
-// import { AppConfig } from '../../../app.config';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class AuthenticationService {
-    // constructor(private http: Http, private config: AppConfig) { }
+
+    private loggedIn = new BehaviorSubject<boolean>(false);
+
     constructor(private http: HttpClient) { }
 
-    login(username: string, password: string) {
-        return this.http.post('http://localhost:4200/api/user/authenticate', { username: username, password: password });
+    get isLoggedIn() {
+        return this.loggedIn.asObservable();
+    }
 
+    login(username: string, password: string) {
+        this.loggedIn.next(true);
+        return this.http.post('http://localhost:4200/api/user/authenticate', { username: username, password: password });
     }
 
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
+        this.loggedIn.next(false);
     }
-
-
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AuthenticationService } from '../auth/authentication/authentication.service';
 
 @Component({
   selector: 'tskr-navbar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: Observable<boolean>;
+
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn;
+  }
+
+  onLogout() {
+    this.authService.logout(); // TODO
   }
 
 }
