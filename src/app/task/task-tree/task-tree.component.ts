e419b05c0b5e5a39fa358480a30afd6025ca35d7
KamKozik <kamkozicki@gmail.com>
2017-11-23 23:20:22 +0100

    Partly working filters

--- src/app/task/task-tree/task-tree.component.ts
+++ src/app/task/task-tree/task-tree.component.ts
@@ -16,6 +16,7 @@ import { User } from '../../users/user/user';
 export class TaskTreeComponent implements OnInit {
 
 
+
   filters: TreeVisible[];
   list: Task[];
   filterdList: FilteredTask[];
@@ -23,75 +24,115 @@ export class TaskTreeComponent implements OnInit {
   MilestonesFilters: IControlPoint[];
 
   constructor(private _route: ActivatedRoute,
-              private _taskService: TaskService,
-              private _taskFiltersService:TaskFiltersService) {
-                this.UsersFilters = new Array();
-                this.MilestonesFilters= new Array();
-               }
+    private _taskService: TaskService,
+    private _taskFiltersService: TaskFiltersService) {
+    this.UsersFilters = new Array();
+    this.MilestonesFilters = new Array();
+
+  }
 
   ngOnInit() {
     // this.list = this._route.snapshot.data['tasks'];
-    this._taskService.getTasksDb().subscribe(res => {this.list = res;
+    this._taskService.getTasksDb().subscribe(res => {
+    this.list = res;
       console.log(this.list);
     });
-    this._taskFiltersService.SharedList$.subscribe(lst =>{ 
+    this._taskFiltersService.SharedList$.subscribe(lst => {
       this.UsersFilters = lst;
 
-      if(this.list!=undefined)this.buildVisibilityTree();
+      if (this.list != undefined) {
+        this.buildVisibilityTree();
+        console.log(this.filters);
+      }
     });
-    this._taskFiltersService.SharedList2$.subscribe(lst =>{
+    this._taskFiltersService.SharedList2$.subscribe(lst => {
       this.MilestonesFilters = lst;
-      if(this.list!=undefined) this.buildVisibilityTree();
+      if (this.list != undefined) {
+        this.buildVisibilityTree();
+        console.log(this.filters);
+      }
 
-     });
+    });
   }
 
-
-
-  buildVisibilityTree(){
-    this.filters=new Array<TreeVisible>(this.list.length) ;
-    this.list.forEach((task,index) => {
-      if(task.children.length>0){
-        console.log(this.filters)
-        this.filters[index]= new TreeVisible();
-        this.filters[index].childrens= new Array<TreeVisible>(task.children.length);
-        this.filters[index].visible= this.makeVisibilityTree(task.children,this.filters[index].childrens);
-        
-      }else{
-        this.filters[index].visible=  this.checkUsersFilters(task) && this.checkMilestonesFilters(task);
+  buildVisibilityTree() {
+    this.filters = new Array<TreeVisible>(this.list.length);
+    let flag: boolean = false;
+    this.list.forEach((task, index) => {
+      if (task.children.length > 0) {
+        let vis: boolean;
+        this.filters[index] = new TreeVisible();
+        this.filters[index].childrens = new Array<TreeVisible>(task.children.length);
+        this.filters[index].visible = vis = this.makeVisibilityTree(task.children, this.filters[index].childrens);
+        flag = vis || flag;
+        if (!flag && index === task.children.length - 1)
+          this.filters[index].visible = this.checkUsersFilters(task) && this.checkMilestonesFilters(task);
+      } else {
+        this.filters[index] = new TreeVisible();
+        this.filters[index].visible = this.checkUsersFilters(task) && this.checkMilestonesFilters(task);
       }
     });
   }
 
-  makeVisibilityTree(list:Task[], filters:TreeVisible[]):boolean{
-    list.forEach((task,index) => {
-      if(task.children.length>0){
-        filters[index]= new TreeVisible();
+  makeVisibilityTree(list: Task[], filters: TreeVisible[]): boolean {
+
+    let flag: boolean = false;
+    let vis: boolean;
+    list.forEach((task, index) => {
+      if (task.children.length > 0) {
+        filters[index] = new TreeVisible();
         filters[index].childrens = new Array<TreeVisible>(task.children.length);
         
-        return filters[index].visible= this.makeVisibilityTree(task.children,filters[index].childrens);
-      }else{
-        filters[index]= new TreeVisible();
-        return filters[index].visible=  this.checkUsersFilters(task) && this.checkMilestonesFilters(task);
+        filters[index].visible = vis = this.makeVisibilityTree(task.children, filters[index].childrens);
+        flag = vis || flag;
+        if (!flag && index === task.children.length - 1) {
+          flag = this.checkUsersFilters(task) && this.checkMilestonesFilters(task);
+        }
+      } else {
+        filters[index] = new TreeVisible();
+        vis = filters[index].visible = this.checkUsersFilters(task) && this.checkMilestonesFilters(task);
+        flag = vis || flag;
       }
-    });
-    return;
-  }
-
-
-  checkUsersFilters(task:Task): boolean {
-    let flag:boolean = true;
-    this.UsersFilters.forEach(user => {
-      if(!task.taskPerformers.includes(user)) flag=false;
     });
     return flag;
   }
 
 
-  checkMilestonesFilters(task:Task): boolean {
-    let flag:boolean = true;
+  checkUsersFilters(task: Task): boolean {
+    if (this.UsersFilters.length === 0 && task.taskPerformers.length > 0) {
+      return true;
+    }
+    if (this.UsersFilters.length > 0 && task.taskPerformers.length === 0) {
+      return false;
+    }
+    if (this.UsersFilters.length === 0 && task.taskPerformers.length === 0) {
+      return true;
+    }
+    let flag: boolean = true;
+    this.UsersFilters.forEach(user => {
+      for (let i = 0; i < task.taskPerformers.length; i++) {
+        if (task.taskPerformers[i].id !== user.id && i === task.taskPerformers.length - 1) flag = false;
+      }
+    });
+    return flag;
+  }
+
+
+  checkMilestonesFilters(task: Task): boolean {
+    if (this.MilestonesFilters.length === 0 && task.controlPointIds.length > 0) {
+      return true;
+    }
+    if (this.MilestonesFilters.length > 0 && task.controlPointIds.length === 0) {
+      return false;
+    }
+    if (this.MilestonesFilters.length === 0 && task.controlPointIds.length === 0) {
+      return true;
+    }
+    let flag: boolean = true;
     this.MilestonesFilters.forEach(milestone => {
-      if(!task.controlPointIds.includes(milestone)) flag=false;
+      for (let i = 0; i < task.controlPointIds.length; i++) {
+        if (task.controlPointIds[i].id !== milestone.id && i === task.controlPointIds.length - 1) flag = false;
+      }
     });
     return flag;
   }
@@ -102,8 +143,8 @@ export class TaskTreeComponent implements OnInit {
 
 class TreeVisible {
   visible: boolean;
-  childrens : TreeVisible[]=[];
-  constructor(){
+  childrens: TreeVisible[] = [];
+  constructor() {
 
   }
 }
