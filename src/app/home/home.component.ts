73f9e95c1c80f02d69ba8dbd96f8460996275708
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-12-05 16:52:16 +0100

    Add onclick on user avatar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,33 +4,30 @@ import { Router } from '@angular/router';
 import { ProjectService } from './../project/shared/project.service';
 import { ProjectModel } from './../project/shared/project.model';
 import { Component, OnInit } from '@angular/core';
-import {  UserService } from '../users/user/user.service';
-import {  User } from '../users/user/user';
-
-
-
+import { UserService } from '../users/user/user.service';
+import { User } from '../users/user/user';
 
 @Component({
-  selector: 'tskr-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'tskr-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
     userProjects: ProjectModel[] = [];
-    
+
     constructor(private userService: UserService,
-                private projectService: ProjectService,
-                private router: Router,
-                public dialog: MatDialog) {
+        private projectService: ProjectService,
+        private router: Router,
+        public dialog: MatDialog) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
         this.loadAllUsers();
         this.projectService.getProjectsFromUser(this.currentUser.id)
-                .subscribe(res => {this.userProjects = res; console.log(res); });
+            .subscribe(res => { this.userProjects = res; });
     }
 
     deleteUser(_id: string) {
@@ -53,19 +50,22 @@ export class HomeComponent implements OnInit {
         const dialogRef = this.dialog.open(ProjectAddComponent, {
             width: '480px',
             // data: { taskId: this.TaskID}
-          });
-  
-          dialogRef.afterClosed().subscribe(result => {
-              if (result) {
-                  this.userProjects.push(result);
-              }
-          });
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.userProjects.push(result);
+            }
+        });
     }
 
     deleteProject(project: ProjectModel) {
         const index = this.userProjects.indexOf(project, 0);
         this.userProjects.splice(index, 1);
         this.projectService.deleteProject(project.id).subscribe();
+    }
 
+    changeAvatar() {
+        console.log('test');
     }
 }
