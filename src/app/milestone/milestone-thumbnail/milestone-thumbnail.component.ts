927726a9b17b75da3e1416153f4f3571fed7d9d3
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-12-05 22:07:15 +0100

    Milestone timeline outline

---
+++ src/app/milestone/milestone-thumbnail/milestone-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tskr-milestone-thumbnail',
+  templateUrl: './milestone-thumbnail.component.html',
+  styleUrls: ['./milestone-thumbnail.component.scss']
+})
+export class MilestoneThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
