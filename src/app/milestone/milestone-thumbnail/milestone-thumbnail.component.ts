32964e76db51981ca1f8a42b2f0924b7f03dfb0f
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-12-06 19:41:34 +0100

    Add actions

--- src/app/milestone/milestone-thumbnail/milestone-thumbnail.component.ts
+++ src/app/milestone/milestone-thumbnail/milestone-thumbnail.component.ts
@@ -1,29 +1,56 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MilestoneModel } from '../shared/milestone-model';
 import * as moment from 'moment';
+import { MilestoneEditComponent } from '../milestone-edit/milestone-edit.component';
+import { MilestoneService } from '../shared/milestone.service';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
 @Component({
-  selector: 'tskr-milestone-thumbnail',
-  templateUrl: './milestone-thumbnail.component.html',
-  styleUrls: ['./milestone-thumbnail.component.scss']
+    selector: 'tskr-milestone-thumbnail',
+    templateUrl: './milestone-thumbnail.component.html',
+    styleUrls: ['./milestone-thumbnail.component.scss']
 })
-export class MilestoneThumbnailComponent implements OnInit {
+export class MilestoneThumbnailComponent implements OnInit, OnChanges {
 
-  @Input() milestone: MilestoneModel;
+    @Input() milestone: MilestoneModel;
 
-  dayOfWeek: string;
-  day: string;
-  monthAndYear: string;
+    dayOfWeek: string;
+    day: string;
+    monthAndYear: string;
 
-  constructor() { }
+    constructor(private milestoneService: MilestoneService, public dialog: MatDialog, public snackBar: MatSnackBar) { }
 
-  ngOnInit() {
-    moment.locale('pl');
-    const formattedDate = moment(this.milestone.endDate).format('LLLL');
-    const splitted = formattedDate.split(' ');
-    this.dayOfWeek = splitted[0].slice(0, -1);
-    this.day = splitted[1];
-    this.monthAndYear = `${splitted[2]} ${splitted[3]}`;
-  }
+    ngOnInit() {
+        moment.locale('pl');
+        const formattedDate = moment(this.milestone.endDate).format('LLLL');
+        const splitted = formattedDate.split(' ');
+        this.dayOfWeek = splitted[0].slice(0, -1);
+        this.day = splitted[1];
+        this.monthAndYear = `${splitted[2]} ${splitted[3]}`;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+
+    }
+
+    editMilestone() {
+        const dialogRef = this.dialog.open(MilestoneEditComponent, {
+            width: '450px',
+            data: { milestone: this.milestone }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.milestoneService.editMilestone(result).subscribe(() => {
+                    this.milestone = result;
+                });
+                this.snackBar.open('Zakończono edycje punktu kontrolnego', '', { duration: 2000 });
+            }
+        });
+    }
+
+    deleteMilestone() {
+        
+    }
 
 }
--- src/app/milestone/milestone.module.ts
+++ src/app/milestone/milestone.module.ts
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
 import { MilestoneComponent } from './milestone.component';
 import { MilestoneThumbnailComponent } from './milestone-thumbnail/milestone-thumbnail.component';
 import { MilestoneAddComponent } from './milestone-add/milestone-add.component';
+import { MilestoneEditComponent } from './milestone-edit/milestone-edit.component';
+import { MilestoneDeleteComponent } from './milestone-delete/milestone-delete.component';
 
 @NgModule({
   imports: [
@@ -14,10 +16,11 @@ import { MilestoneAddComponent } from './milestone-add/milestone-add.component';
     TaskerMaterialModule
   ],
   providers: [MilestoneService],
-  declarations: [MilestoneComponent, MilestoneThumbnailComponent, MilestoneAddComponent],
-  exports: [MilestoneComponent, MilestoneThumbnailComponent, MilestoneAddComponent],
+  declarations: [MilestoneComponent, MilestoneThumbnailComponent, MilestoneAddComponent, MilestoneEditComponent],
+  exports: [MilestoneComponent, MilestoneThumbnailComponent, MilestoneAddComponent, MilestoneEditComponent],
   entryComponents: [
-    MilestoneAddComponent
+    MilestoneAddComponent,
+    MilestoneEditComponent
   ]
 })
 export class MilestoneModule { }
